57587199e5b1b45f035a67e75259e65609ee7ee0
yusufadji <admin@frelein.my.id>
2022-06-06 12:17:29 +0700

    Perbaiki layout checkout

--- app/src/main/java/com/uty/clothstore/CheckoutActivity.kt
+++ app/src/main/java/com/uty/clothstore/CheckoutActivity.kt
@@ -2,10 +2,38 @@ package com.uty.clothstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
+import java.text.NumberFormat
+import java.util.*
 
 class CheckoutActivity : AppCompatActivity() {
+    private lateinit var totalBayar: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+
+        totalBayar = findViewById(R.id.checkout_total_bayar)
+        val totalan = intent.getDoubleExtra("total", 0.0)
+
+        totalBayar.text = rupiah(totalan.toInt())
+        val type = arrayOf("DANA", "OVO", "GoPay")
+
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.menu_payment,
+            type
+        )
+
+        val editTextFilledExposedDropdown = findViewById<AutoCompleteTextView>(R.id.filled_exposed_dropdown)
+        editTextFilledExposedDropdown.setAdapter(adapter)
     }
+}
+
+private fun rupiah(number: Int): String {
+    val localeID = Locale("in", "ID")
+    val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+    numberFormat.maximumFractionDigits = 0
+    return numberFormat.format(number)
 }
--- app/src/main/java/com/uty/clothstore/KeranjangActivity.kt
+++ app/src/main/java/com/uty/clothstore/KeranjangActivity.kt
@@ -12,6 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.uty.clothstore.adapter.KeranjangRVAdapter
 import com.uty.clothstore.model.KeranjangRVModel
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class KeranjangActivity : AppCompatActivity() {
     private lateinit var keranjangView: RecyclerView
@@ -34,36 +37,6 @@ class KeranjangActivity : AppCompatActivity() {
         loadingKrj = findViewById(R.id.loading_keranjang)
         loadingKrj.visibility = View.VISIBLE
         keranjangView = findViewById(R.id.keranjang_rv)
-//        keranjangList.add(
-//            KeranjangRVModel(
-//                "FRAGILE - Oversized Wool Sweater Dewasa Elegan Sederhana",
-//                10000,
-//                "https://ksufmvei.sirv.com/cloth-store/m62335136641_1.jpg",
-//                1))
-//        keranjangList.add(
-//            KeranjangRVModel(
-//                "Vivienne Westwood Red Label Denim",
-//                10000,
-//                "https://static.mercdn.net/item/detail/orig/photos/m13451739216_1.jpg?1643179806",
-//                1))
-//        keranjangList.add(
-//            KeranjangRVModel(
-//                "L'EST ROSE Gaun Pola Bunga Kotak-kotak Musim Semi / Musim Panas",
-//                10000,
-//                "https://static.mercdn.net/item/detail/orig/photos/m43540024457_1.jpg?1652407185",
-//                1))
-//        keranjangList.add(
-//            KeranjangRVModel(
-//                "HERILL 22ss Jacket Weekend NEW Warna Blue Navy",
-//                10000,
-//                "https://static.mercdn.net/item/detail/orig/photos/m85062866189_1.jpg?1650211616",
-//                1))
-//        keranjangList.add(
-//            KeranjangRVModel(
-//                "L'EST ROSE Dress Hitam Lipit Renda Kamisol Ukuran S",
-//                10000,
-//                "https://static.mercdn.net/item/detail/orig/photos/m10322858278_1.jpg?1651474675",
-//                1))
 
         val tvTotal = findViewById<TextView>(R.id.keranjang_total)
         keranjangList = MyApplication.getSemuaKeranjang(this)!!
@@ -77,17 +50,25 @@ class KeranjangActivity : AppCompatActivity() {
             layoutManager = keranjangViewManager
         }
 
-        btnCheckout.setOnClickListener{
-            val intent = Intent(this, CheckoutActivity::class.java)
-            startActivity(intent)
-        }
-
         var total: Double = 0.0
         for (item in keranjangList) {
             total += item.produkHargaDiskon * item.produkQty
         }
 
-        tvTotal.text = "Rp. " + total.toInt()
+        tvTotal.text = rupiah(total.toInt())
         loadingKrj.visibility = View.GONE
+
+        btnCheckout.setOnClickListener{
+            val intent = Intent(this, CheckoutActivity::class.java)
+            intent.putExtra("total", total)
+            startActivity(intent)
+        }
     }
+}
+
+private fun rupiah(number: Int): String {
+    val localeID = Locale("in", "ID")
+    val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+    numberFormat.maximumFractionDigits = 0
+    return numberFormat.format(number)
 }
